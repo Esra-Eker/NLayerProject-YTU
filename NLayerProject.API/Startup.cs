007cd1778f3b2f4c3f0244e830ecbd29f8358a50
Esra <elifesraeker@gmail.com>
2022-03-29 15:30:18 +0300

    Swagger İşlemleri Yapıldı.

--- NLayerProject.API/Startup.cs
+++ NLayerProject.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,23 @@ namespace NLayerProject.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region Swagger Ýþlemleri
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "1.0.0",
+                    Title = "API Swagger",
+                    Description = "Api Swagger Documentation",
+                    TermsOfService = new Uri("http://swagger.io/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "YTU IEEE Group"
+                    }
+                });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +64,14 @@ namespace NLayerProject.API
             {
                 endpoints.MapControllers();
             });
+            #region Swagger Ýþlemleri
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", " API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            #endregion
         }
     }
 }
