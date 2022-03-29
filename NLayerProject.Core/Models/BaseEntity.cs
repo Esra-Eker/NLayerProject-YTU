be922325a6f11bccc2d9409a279f162ce48aa256
Esra <elifesraeker@gmail.com>
2022-03-29 15:43:02 +0300

    BaseEntity yapısı oluşturuldu

---
+++ NLayerProject.Core/Models/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerProject.Core.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
